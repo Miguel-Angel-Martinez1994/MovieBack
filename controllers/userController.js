4dd818dd3e71f9b4f24f102905e1219aee7104f8
Miguel Martinez <mamg1603@gmail.com>
2024-10-30 15:44:41 +0100

    UPDATE:usuarios model y controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const {peliculasByTittle}=require("../models/movieModels")
+const {
+    favoritosLista,
+    favoritoDetalles,
+    favoritoCrear
+}=require("../models/userModel")
 
 const get_pelicula_by_tittle_controller=async(req,res)=>{
     let {titulo}=req.params
@@ -19,10 +24,39 @@ const get_pelicula_by_tittle_controller=async(req,res)=>{
 }
 
 const get_detalle_pelicula_controller=async(req,res)=>{
+    let detallesPelicula=req.boby
+    try{
+        const result=await favoritoDetalles(detallesPelicula)
+        return res.status(200).json({
+            ok:true,
+            msg:"mostrando favorito",
+            result
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            msg:"Error al mostrar detalles del favorito"
+        })
+    }
 
 }
 
 const get_peliculas_favoritas=async(req,res)=>{
+    let identificador=req.body
+
+    try{
+        const result=await favoritosLista(identificador)
+        return res.status(200).json({
+            ok:true,
+            msg:"mostrando lista favoritos",
+            result
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            msg:"Error al mostrar lista de favoritos"
+        })
+    }
 
 }
 
--- models/userModel.js
+++ models/userModel.js
@@ -17,13 +17,27 @@ const favoritosLista=async(usuario)=>{
 }
 
 
-const favoritoDetalles=async()=>{
+const favoritoDetalles=async(data)=>{
     //recibir usuario_id y pelicula_id
+    const {usuario_id,pelicula_id}=data
+    try{
+        const respuesta=await connect(favoritos.detallesFavorito,[usuario_id,pelicula_id])
+        return respuesta
+    }catch(error){
+        console.log(error)
+    }
 }
 
 
-const favoritoCrear=async()=>{
-
+const favoritoCrear=async(data)=>{
+    //recibir usuario_id y pelicula_id
+    const {usuario_id,pelicula_id}=data
+    try{
+        const respuesta=await connect(favoritos.crearFavorito,[usuario_id,pelicula_id])
+        return respuesta
+    }catch(error){
+        console.log(error)
+    }
 }
 
 module.exports={
